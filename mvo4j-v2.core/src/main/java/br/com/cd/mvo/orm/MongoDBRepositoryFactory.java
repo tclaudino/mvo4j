c2c4b499cfd46218f9dd0eb3160c808c10ca97f6
Isaac Silva <isaac.taborda@gmail.com>
2014-04-27 05:48:30 -0300

    adcionando suporte ao mongodb

---
+++ mvo4j-v2.core/src/main/java/br/com/cd/mvo/orm/MongoDBRepositoryFactory.java
@@ -0,0 +1,38 @@
+package br.com.cd.mvo.orm;
+
+import java.lang.annotation.Annotation;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import br.com.cd.mvo.core.RepositoryMetaData;
+import br.com.cd.mvo.ioc.ConfigurationException;
+import br.com.cd.mvo.ioc.Container;
+import br.com.cd.mvo.ioc.NoSuchBeanDefinitionException;
+
+public class MongoDBRepositoryFactory extends AbstractRepositoryFactory<MongoOperations, MongoOperations, MongoRepository<?>> {
+
+	public static final Class<? extends Annotation> PERSISTENCE_TYPE_ANNOTATION = Document.class;
+	public static final Class<? extends Annotation> PERSISTENCE_IDENTIFIER_ANNOTATION = Id.class;
+
+	public MongoDBRepositoryFactory(Container container) {
+		super(container, PERSISTENCE_TYPE_ANNOTATION, PERSISTENCE_IDENTIFIER_ANNOTATION);
+	}
+
+	@Override
+	protected MongoOperations createPersistenceManager(MongoOperations factory) throws NoSuchBeanDefinitionException {
+
+		return factory;
+	}
+
+	@Override
+	public <T> MongoRepository<T> getInstance(RepositoryMetaData<T> metaData) throws ConfigurationException {
+
+		MongoOperations em = this.getPersistenceManager(metaData.persistenceManagerQualifier());
+		MongoRepositoryImpl<T> repositoryImpl = new MongoRepositoryImpl<>(em, metaData);
+
+		return repositoryImpl;
+	}
+
+}
